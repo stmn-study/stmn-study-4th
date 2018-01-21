35e1a775594a640e5b4e2405d46cf8f4bf71fccb
matsutani <yushiro.ma2ta2.21@gmail.com>
2018-01-21 09:50:33 +0900

    multipleのfile_tag対応

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  has_many_attached :images
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_01_20_115205) do
+ActiveRecord::Schema.define(version: 2018_01_20_123001) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2018_01_20_115205) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
